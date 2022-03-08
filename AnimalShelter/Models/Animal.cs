ecc1b280e37de83292b02ca9c51e418c1586b048
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-03-08 15:27:30 -0800

    Add files for animal shelter using EF core
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,13 @@
+using System;
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public DateTime AdmitDate { get; set; }
+    public string Breed { get; set; }
+  }
+}
